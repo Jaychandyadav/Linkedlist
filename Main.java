090f12ef98d605b5ebfdc09ced991f91fbdb88e9
Jay Chand Yadav <73188396+Jaychandyadav@users.noreply.github.com>
2021-05-21 17:20:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+ // -----  Insertion begin & any position & Deletion begin,end,& any position ----
  class Main
  {
 	 static class Node
@@ -70,6 +71,48 @@
 		{
 			System.out.println("Length is: " + length);
 		}
+		
+		public void del_begin()
+		{
+			head=head.next;
+		}
+		
+		public void del_end()
+		{
+			length--;
+			Node t = head;
+			while(t.next.next!=null)
+			{
+				t=t.next;
+			}
+			t.next=null;
+		}
+		
+		public void del_pos(int p)
+		{
+			
+			if(head==null)
+			{
+				System.out.println("Underflow");
+				
+			}
+			
+			else if(head.next==null)
+			{
+				length--;
+				head = null;
+			}
+			
+			else
+			{
+				Node d = head;
+				for(int j=1;j<p-1;j++)
+				{
+					d = d.next;
+				}
+				d.next = d.next.next;
+			}
+		}
 	 }
 	 
 	public static void main(String[] args)
@@ -83,6 +126,13 @@
 		ll.insert(6);
 		ll.insertbegin(0);
 		ll.insertmid(1,2);
+		ll.show();
+		System.out.println("Again printing");
+		ll.del_begin();
+		ll.del_end();
+		ll.show();
+		System.out.println("Re-Again printing");
+		ll.del_pos(2);
 		ll.show();
 		ll.length();
 		
